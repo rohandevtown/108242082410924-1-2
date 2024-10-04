d0c9ae2bf187c2c08baa8a49aaa6108ace78fefb
rohandevtown <rohankinnal@devtown.in>
2024-10-04 19:16:23 +0530

    d3

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
-console.log("hello world")
+// console.log("hello world")
+
+// const state = {
+//     taskList: [
+//         {
+//             url: "",
+//             title: "",
+//             type: "",
+//             description: ""
+//         },
+//         {
+//             url: "",
+//             title: "",
+//             type: "",
+//             description: ""
+//         },
+//         {
+//             url: "",
+//             title: "",
+//             type: "",
+//             description: ""
+//         }
+//     ]
+// }
+
+const state = {
+    taskList: []
+}
+
+
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal_body");
+// console.log(taskModal)
+
+
+const htmlTaskContent = ({id, url, title, description, type}) => `
+    <div class="col-md-6 col-lg-4 mt-3" id="${id}" key="${id}">
+      <div class="card">
+        <div class="card-header d-flex justify-content-end gap-2">
+          <button type="button" class="btn btn-outline-primary" name="${id}">
+            <i class="fa-solid fa-pencil"></i>
+          </button>
+          <button type="button" class="btn btn-outline-danger" name="${id}">
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+        <div class="card-body">
+          ${ url && `<img
+            src="${url}"
+            alt="card-img-top"
+            class="card-img-top"
+          />` }
+          <h4 class="card-title">${title}</h4>
+          <p class="card-text">${description}</p>
+          <div class="tags d-flex flex-wrap">
+            <span class="badge bg-primary m-1">${type}</span>
+          </div>
+        </div>
+        <div class="card-footer">
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#showTask"
+          >
+            Open Task
+          </button>
+        </div>
+      </div>
+    </div>
+`
+
+
+const htmlModalContent = ({id, url, title, description}) => {
+    const date = new Date();
+    return `
+        <div id=${id}>
+            ${ url && `<img
+            src="${url}"
+            alt="card-img-top"
+            class="img-fluid"
+          />` }
+          <strong>Created on ${date.toDateString()}</strong>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </div>
+    `
+}
